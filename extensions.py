51b888ff4a3ed2f9ea5581440a141a468c149c32
Dimonn1206 <142986154+Dimonn1206@users.noreply.github.com>
2023-12-22 01:24:43 +0300

    Add files via upload

---
+++ extensions.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from config import keys
+
+
+class ConvertionExcepption(Exception):
+    pass
+
+
+class CryptoConverter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionExcepption(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionExcepption(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionExcepption(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionExcepption(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base
